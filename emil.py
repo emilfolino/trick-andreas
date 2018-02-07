8959ea619ace8ed336541fb7461b5181821758ab
Kenneth Lewenhagen <lewenhagen@live.com>
2018-02-07 15:30:36 +0100

    This commit is a lie

--- emil.py
+++ emil.py
@@ -11,6 +11,6 @@ def a(n):
 
 b = [x for x in range(2, 100) if a(x)]
 
-c = [b[z]**2 + b[z+1]**2 for z in range(len(b) - 1)]
+c = [math.sqrt(b[z]**2 + b[z+1]**2) for z in range(len(b) - 1)]
 
 print(c)
