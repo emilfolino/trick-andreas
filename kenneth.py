be1b426859d2a2ca5fc085c9fbe435753afe4599
Kenneth Lewenhagen <lewenhagen@live.com>
2018-02-07 16:02:08 +0100

    wrong id in server

---
+++ kenneth.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+"""
+Main file for something special
+"""
+
+import random
+
+d = ["", "morot.readline", "Thesis Projects", ".strip", "morot"]
+path = "abcd.txt"
+pancake = "Since our file has been opened"
+stop = 0
+beginning_of_the_end = -45
+falafel = [None] * 5
+nose_spray = d[3:4]
+with open(path, "r") as morot:
+    d[4] = "dont mind me"
+    pancake = eval(d[1] + "()" + nose_spray[0] + "()")
+    #print(pancake)
+
+for calorie in pancake:
+    cucumber = 1
+    if calorie in (" ", ", ", "."):
+        falafel.append(calorie)
+    else if calorie in ", ":
+        falafel.append(cucumber)
+
+
+    cucumber += 1
+
+falafel = falafel[5:]
+print(falafel)
